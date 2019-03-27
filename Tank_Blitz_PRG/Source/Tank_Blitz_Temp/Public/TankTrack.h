d615ede0df7b0df7f2cda897073ebbda9beb2c34
zyoKo <vatsalya.yd@gmail.com>
2019-03-28 00:49:25 +0530

    Adding Movement and changing Friction Setting

---
+++ Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/TankTrack.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class TANK_BLITZ_TEMP_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Set throttle values between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000;
+};
