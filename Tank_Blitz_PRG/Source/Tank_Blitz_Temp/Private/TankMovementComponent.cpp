bb05940839308d278d87bc573e1f5c9e232d48c2
zyoKo <vatsalya.yd@gmail.com>
2019-03-29 04:21:56 +0530

    AI Akward Tank Movement

--- Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Private/TankMovementComponent.cpp
+++ Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Private/TankMovementComponent.cpp
@@ -11,9 +11,16 @@ void UTankMovementComponent::Initialise(UTankTrack* LeftTankTrackToSet, UTankTra
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString);
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	IntendMoveForward(ForwardThrow);
+
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
+	
+	// UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString);
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
