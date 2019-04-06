eb2b45320096967c1ebb8e2af32105184a6707c9
zyoKo <vatsalya.yd@gmail.com>
2019-04-06 21:22:48 +0530

    Tank Health System UI Complete

--- Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/Tank.h
+++ Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/Tank.h
@@ -14,10 +14,14 @@ class TANK_BLITZ_TEMP_API ATank : public APawn
 public:
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	// returns currenthealth as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = Health)
+	float GetHealthPercent();
+
+private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-private:
 	UPROPERTY(EditDefaultsOnly, Category = Setup)
 	int32 StartingHealth = 100;
 
