1c51c347624ed625416a28043590d39cc7f218f1
zyoKo <vatsalya.yd@gmail.com>
2019-04-06 20:27:55 +0530

    Projectile Editing

--- Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/Projectile.h
+++ Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/Projectile.h
@@ -5,7 +5,10 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "PhysicsEngine/RadialForceComponent.h"
 #include "Projectile.generated.h"
+
+class UParticleSystemComponent;
 
 UCLASS()
 class TANK_BLITZ_TEMP_API AProjectile : public AActor
@@ -16,9 +19,31 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 	void LaunchProjectile(float Speed);
 
 private:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	void OnTimerExpire();
+
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float DestroyDelay = 10.f;
+
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Components)
+	UStaticMeshComponent* CollisionMesh = nullptr;
 	
+	UPROPERTY(VisibleAnywhere, Category = Components)
+	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Components)
+	UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Components)
+	URadialForceComponent* ExplosionForce = nullptr;
 };
--- Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/TankAimingComponent.h
+++ Tank_Blitz_PRG/Source/Tank_Blitz_Temp/Public/TankAimingComponent.h
@@ -69,9 +69,10 @@ private:
 	UPROPERTY(EditDefaultsOnly, Category = Firing)
 	float ReloadTimeInSeconds = 3;
 
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	int32 RoundsLeft = 3;
+
 	double LastFireTime = 0;
 
 	FVector AimDirection;
-
-	int RoundsLeft = 3;
 };
